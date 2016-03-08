62e25afcdc6c0e55e41c252a547e1cc5eab73c22
Brian Vanderbusch <brian@hackerhappyhour.com>
2016-03-07 19:55:33 -0600

    add doc for cli config override

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ var server = new Hapi.Server();
 
 /**
 * `host` and `port` are pulled from `package.json`'s config keys,
-* and can be overridden one of three ways:
+* and can be overridden one of 4 ways:
 *    - by running `npm config set @h3/impact:port 80`
 *    - setting NPM_CONFIG_PORT=80
 *    - place a `.npmrc` in the root of the project with  `port = 80` on a line
+*    - add a command line param `node ./index.js --port 80`
 **/
 server.connection({
   host: process.env.npm_package_config_host,
