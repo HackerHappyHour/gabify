8f1088c2ec9cc51c61958d7d87c8d05943673b94
Brian Vanderbusch <brian@hackerhappyhour.com>
2016-03-14 21:02:57 -0500

    basic cycle setup

--- src/app/index.js
+++ src/app/index.js
@@ -1,2 +1,16 @@
 var {run} = require("@cycle/core");
-var {makeDOMDriver} = require("@cycle/dom");
+var {div, makeDOMDriver} = require("@cycle/dom");
+
+function main(sources){
+  return {
+    DOM: Rx.Observable.of(
+      div(['Welcome to impact team chat'])
+    )
+  };
+};
+
+var drivers = {
+  DOM: makeDOMDriver('#app')
+};
+
+run(main, drivers);
